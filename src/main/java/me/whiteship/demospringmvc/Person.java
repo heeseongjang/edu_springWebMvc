3defb15aec8b219a08d2e66140c6c6991be0c996
heeseong <wkdgmltjd1865@gmail.com>
2022-03-01 21:09:14 +0900

    스프링 MVC 소개

---
+++ src/main/java/me/whiteship/demospringmvc/Person.java
@@ -0,0 +1,14 @@
+package me.whiteship.demospringmvc;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Person {
+    private String name;
+}
