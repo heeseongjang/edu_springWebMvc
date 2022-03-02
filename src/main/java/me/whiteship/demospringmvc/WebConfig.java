f9acdb8b0291a5999bc985bd54ad8cfaeda55b77
heeseong <wkdgmltjd1865@gmail.com>
2022-03-02 22:36:44 +0900

    리소스 핸들러

--- src/main/java/me/whiteship/demospringmvc/WebConfig.java
+++ src/main/java/me/whiteship/demospringmvc/WebConfig.java
@@ -1,8 +1,11 @@
 package me.whiteship.demospringmvc;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.http.CacheControl;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -11,11 +14,19 @@ public class WebConfig implements WebMvcConfigurer {
 //        registry.addFormatter(new PersonFormatter());
 //    }
 
+//    @Override
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(new GreetingInterceptor()).order(0);
+//        registry.addInterceptor(new AnotherInterceptor())
+//                .addPathPatterns("/hi")
+//                .order(-1);
+//    }
+
+
     @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new GreetingInterceptor()).order(0);
-        registry.addInterceptor(new AnotherInterceptor())
-                .addPathPatterns("/hi")
-                .order(-1);
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/mobile/**")
+                .addResourceLocations("classpath:/mobile/")
+                .setCacheControl(CacheControl.maxAge(10, TimeUnit.MINUTES));
     }
 }
